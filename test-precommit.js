5ff57c3226ea65a8f7684cd79a0860c2a0ab4c18
William Zhou <williamz.zhou1@gmail.com>
2020-12-14 03:14:59 -0800

    Implement pre-commit lint and format via lint-stage and husky

---
+++ test-precommit.js
@@ -0,0 +1,9 @@
+const formatThis = 'should be single quote';
+const shouldHaveTrailingComma = [1, 2, 3];
+const shouldAlsoHaveTrailingComma = { dog: 'hat' };
+
+module.exports = {
+  formatThis,
+  shouldAlsoHaveTrailingComma,
+  shouldHaveTrailingComma,
+};
