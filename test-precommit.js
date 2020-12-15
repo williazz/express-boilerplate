33fa7d0cdc77e454b2f6339118bdb8f06d0bf9c6
William Zhou <williamz.zhou1@gmail.com>
2020-12-14 17:04:56 -0800

    Removed pre-commit test file

--- test-precommit.js
+++
@@ -1,9 +0,0 @@
-const formatThis = 'should be single quote';
-const shouldHaveTrailingComma = [1, 2, 3];
-const shouldAlsoHaveTrailingComma = { dog: 'hat' };
-
-module.exports = {
-  formatThis,
-  shouldAlsoHaveTrailingComma,
-  shouldHaveTrailingComma,
-};
